b5c1d438d1407d88a5e185010c49a7652972d9c6
Rob <robthomp@uw.edu>
2015-02-09 14:53:51 -0800

    Got the stack trace working finally by injecting code into the document then sending the event data back to the content script as messages.

--- frame.js
+++ frame.js
@@ -3,19 +3,34 @@
 //	frames[i].style.display="none";
 //}
 
-function getStackTrace() {
-  var obj = {};
-  Error.captureStackTrace(obj, getStackTrace);
-  return obj.stack;
+// This code will be injected to run in webpage context
+function catchDOMChanges() {
+    var counter = 1;
+    document.addEventListener('DOMNodeInserted', function(e) {
+        //give the node an ID but ONLY if it doesn't have one already
+        e.target.id = (e.target.id || "extension_marker_id_" + counter++);
+        var i = {
+            stack: (new Error().stack),
+            id: e.target.id
+        };
+        //window.dispatchEvent(new CustomEvent('ReportChange', i));
+        window.postMessage(i, '*');
+    });
 }
 
-function domEvent(e) {
-	//var trace = getStackTrace();
-	console.trace();
-	console.log(e.target);
-	//console.log(trace);
-	$(e.target).css("border", "5px solid red");
-}
+//we just got an event
+window.addEventListener('message', function(e) {
+    //need to parse stack trace and do something with it
+    console.log(e.data);
+    $('#' + e.data.id).css('border', '5px red solid');
+});
 
-document.addEventListener('DOMNodeInserted', domEvent);
+/*window.addEventListener('ReportChange', function(e) {
+    console.log(e);
+});*/
 
+//Inject code
+var script = document.createElement('script');
+script.textContent = '(' + catchDOMChanges + '())';
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
